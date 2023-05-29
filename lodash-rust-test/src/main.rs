df00993fd4492ee073961be286347f51fbc55d31
charleslukes <charleslukes28@gmail.com>
2023-05-29 23:58:02 +0100

    add to capitalize to test app

--- lodash-rust-test/src/main.rs
+++ lodash-rust-test/src/main.rs
@@ -1,7 +1,13 @@
 extern crate lodash_rust;
 use lodash_rust::camel_case;
+use lodash_rust::capitalize;
+
 
 fn main() {
     let result = camel_case::new("xhr2 request".to_string());
-    println!("result ==> {}", result)
+    println!("result ==> {}", result);
+
+    let capitalize = capitalize::new("fred".to_string());
+    println!("result ==> {}", capitalize);
+
 }
