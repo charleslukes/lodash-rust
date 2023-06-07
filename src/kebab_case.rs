48984d99077b72d3ec115488eaade02d0983da45
charleslukes <charleslukes28@gmail.com>
2023-06-07 02:17:22 +0100

    update name

--- src/kebab_case.rs
+++ src/kebab_case.rs
@@ -11,29 +11,29 @@ pub fn new(s: &str) -> String {
     let re = regex::Regex::new("[^a-zA-Z0-9]+").unwrap();
     let result = re.replace_all(s, " ");
     let result = result.trim();
-    let data = re.replace_all(result, "-");
-    
+    let result_string = re.replace_all(result, "-");
+
     // check if string contains "-"
     let re2 = regex::Regex::new("[-]+").unwrap();
-    let contains_hyphen = re2.is_match(data.as_ref());
+    let contains_hyphen = re2.is_match(result_string.as_ref());
 
-    let mut build_data = String::new();
+    let mut build_result_string = String::new();
 
     if !contains_hyphen {
         let mut tmp = [0u8; 4];
         // convert to char
-        let characters: Vec<char> = data.chars().collect();
+        let characters: Vec<char> = result_string.chars().collect();
         for letter in characters {
             if letter.is_uppercase() {
-                build_data.push_str("-");
+                build_result_string.push_str("-");
             }
-            build_data.push_str(letter.encode_utf8(&mut tmp));
+            build_result_string.push_str(letter.encode_utf8(&mut tmp));
         }
 
-        return build_data.to_lowercase();
+        return build_result_string.to_lowercase();
     }
 
-    return data.to_string().to_lowercase();
+    return result_string.to_string().to_lowercase();
 }
 
 #[test]
