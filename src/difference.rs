5c8a67b624f2803195e6d64ac21fc9ccc8ced2e0
charleslukes <charleslukes28@gmail.com>
2023-06-09 16:56:09 +0100

    update method using generics

--- src/difference.rs
+++ src/difference.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::hash::Hash;
 
 /// Creates an array of `array` values not included in the other given arrays
 /// using [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
@@ -10,16 +11,16 @@ use std::collections::HashSet;
 /// @param {Vec<Vec>} [values] The values to exclude.
 /// @returns {Vec} Returns the new vector of filtered values.
 
-// TODO: make i32 a generic 
-pub fn new(array: Vec<i32>, values: Vec<Vec<i32>>) -> Vec<i32> {
+pub fn new<T: PartialOrd + Eq + Hash + Clone>(array: Vec<T>, values: Vec<Vec<T>>) -> Vec<T> {
     let mut new_array = array.clone();
-    
-    let only_unique_values = &values.concat()
+
+    let only_unique_values = &values
+        .concat()
         .into_iter()
         .map(|a| a)
         .collect::<HashSet<_>>()
         .into_iter()
-        .collect::<Vec<i32>>();
+        .collect::<Vec<T>>();
 
     for unique_value in only_unique_values {
         if array.contains(unique_value) {
@@ -36,9 +37,7 @@ pub fn new(array: Vec<i32>, values: Vec<Vec<i32>>) -> Vec<i32> {
 
 #[test]
 fn test_new() {
-    let original_vector =[2, 1, 2, 3].to_vec();
+    let original_vector = [2, 1, 2, 3].to_vec();
     let vector_to_check = [[3, 2].to_vec(), [3, 4].to_vec()].to_vec();
     assert_eq!(new(original_vector, vector_to_check), [1]);
-
-   
 }
