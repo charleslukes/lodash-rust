de0b86cb01f63a9334b9abf7533ed9a27009b47b
charleslukes <charleslukes28@gmail.com>
2023-06-09 18:45:45 +0100

    update comment

--- src/drop.rs
+++ src/drop.rs
@@ -9,6 +9,7 @@ use std::convert::TryInto;
 ///
 
 pub fn new<T: PartialOrd + Eq + Clone>(array: Vec<T>, number: Option<i32>) -> Vec<T> {
+    // defaults to 1
     let num = number.unwrap_or(1);
     let arr_len = array.len().try_into().unwrap();
     if num > arr_len {
