562db24f42838d1ec65089e1d8cd67ec776affda
charleslukes <charleslukes28@gmail.com>
2023-07-16 03:22:00 +0100

    add filter method

---
+++ src/filter.rs
@@ -0,0 +1,12 @@
+pub fn new<T: Copy>(array: Vec<T>, f: &dyn Fn(T) -> bool) -> Vec<T> {
+    let mut result: Vec<T> = Vec::new();
+
+    for index in 0..array.len() {
+        let value = array[index];
+        if f(value) {
+            result.push(value);
+        }
+    }
+
+    return result;
+}
