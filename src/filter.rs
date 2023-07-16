4b5579cd7c02095751b5b6c3af341b914e918dd9
charleslukes <charleslukes28@gmail.com>
2023-07-16 03:33:29 +0100

    add doc

--- src/filter.rs
+++ src/filter.rs
@@ -1,3 +1,10 @@
+///  
+///  Iterates over elements of `array`, returning an array of all elements
+///  `predicate` returns truthy for. The predicate is invoked with three
+///  arguments: (value, index, array).
+///  
+
+
 pub fn new<T: Copy>(array: Vec<T>, f: &dyn Fn(T) -> bool) -> Vec<T> {
     let mut result: Vec<T> = Vec::new();
 
