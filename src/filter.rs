5f0d0188e49468d515e54047c4ff0b0f1f73af72
charleslukes <charleslukes28@gmail.com>
2023-07-17 20:49:45 +0100

    add test case

--- src/filter.rs
+++ src/filter.rs
@@ -17,3 +17,11 @@ pub fn new<T: Copy>(array: Vec<T>, f: &dyn Fn(T) -> bool) -> Vec<T> {
 
     return result;
 }
+
+
+#[test]
+fn test_new() {
+    assert_eq!(new([1, 2, 3, 4].to_vec(), &|x: i32| x < 3), [1, 2]);
+    assert_eq!(new([false, false, false, false].to_vec(), &|x: bool| x), []);
+    assert_eq!(new([false, true, false, false].to_vec(), &|x: bool| !x), [false, false, false]);
+}
