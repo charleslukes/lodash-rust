9bd045530a8887fd8ff8d34a038faf86adb357dc
charleslukes <charleslukes28@gmail.com>
2023-07-17 23:18:04 +0100

    update doc for difference

--- src/difference.rs
+++ src/difference.rs
@@ -1,15 +1,24 @@
+//! Creates an array of `array` values not included in the other given arrays
+//! using [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
+//! 
+//! for equality comparisons. The order and references of result values are
+//! determined by the first array.
+//! 
+//! Example
+//! ```
+//! use lodash_rust::difference;
+//! 
+//! fn main() {
+//!  let original_vector = [2, 1, 2, 3].to_vec();
+//!  let vector_to_check = [[3, 2].to_vec(), [3, 4].to_vec()].to_vec();
+//!  let res = difference::new(original_vector, vector_to_check);
+//!  println!("{res}") // [1]
+//! }
+//! ```
+//!  
+
 use std::collections::HashSet;
 use std::hash::Hash;
-
-/// Creates an array of `array` values not included in the other given arrays
-/// using [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
-/// for equality comparisons. The order and references of result values are
-/// determined by the first array.
-///
-/// @category Vec
-/// @param {Vec}  The vector to inspect.
-/// @param {Vec<Vec>} [values] The values to exclude.
-/// @returns {Vec} Returns the new vector of filtered values.
 
 pub fn new<T: PartialOrd + Eq + Hash + Clone>(array: Vec<T>, values: Vec<Vec<T>>) -> Vec<T> {
     let mut new_array = array.clone();
