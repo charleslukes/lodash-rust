cf46b307aec7d9301ebcb730cccdff44eaa6aac5
charleslukes <charleslukes28@gmail.com>
2023-07-20 03:03:48 +0100

    update method

--- src/find_last_index.rs
+++ src/find_last_index.rs
@@ -1,6 +1,8 @@
 //! This method is like `findIndex` except that it iterates over elements
 //! of `collection` from right to left.
-//!
+//! 
+//! Returns the index of the found element, else None.
+//!  
 //! Example
 //! ```
 //! use lodash_rust::find_last_index;
@@ -22,18 +24,19 @@
 //! let person_3 = Person::new(20, String::from("Luke"));
 //!
 //! fn is_older_than_20(p: &Person) -> bool {
-//!    p.age > 20
+//!    p.age >= 20
 //! }
 //! let all_persons = vec![person_1, person_2, person_3];
 //! let res = find_last_index::new(&all_persons, is_older_than_20)
-//! println!("{res}") // Some(0)
+//! println!("{res}") // Some(2)
 //! }
 //! ```
 
 pub fn new<T, F: Fn(&T) -> bool>(array: &Vec<T>, p: F) -> Option<usize> {
-    for (i, v) in array.iter().enumerate() {
-        if p(&v) {
-            return Some(i);
+    let index_len = array.len() - 1;
+    for (i, _v) in array.iter().enumerate() {
+        if p(&array[index_len - i]) {
+            return Some(index_len - i);
         }
     }
 
@@ -58,10 +61,10 @@ fn test_new() {
     let person_3 = Person::new(20, String::from("Luke"));
 
     fn is_older_than_20(p: &Person) -> bool {
-        p.age > 20
+        p.age >= 20
     }
 
     let all_persons = vec![person_1, person_2, person_3];
 
-    assert_eq!(new(&all_persons, is_older_than_20), Some(0));
+    assert_eq!(new(&all_persons, is_older_than_20), Some(2));
 }
